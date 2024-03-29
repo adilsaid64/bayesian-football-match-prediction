cbd607eacb6e813dc0599b448431052737cf2a07
adilsaid <adilsaid64@gmail.com>
2024-03-29 12:18:36 +0000

    runing the bayesian model. used less features.

--- bayesian-model.r
+++ bayesian-model.r
@@ -16,6 +16,9 @@ X_train <- cbind(1, X_train)
 X_test <- cbind(1, X_test)
 X_future <- cbind(1, X_future)
 
+print(length(X_train))
+print(length(X_test))
+print(length(X_future))
 ###### MODEL TRAINING
 model_string <- 'model{
   for(i in 1:N){
@@ -29,6 +32,7 @@ model_string <- 'model{
     beta[j] ~ dnorm(0, 0.001)
   }
 }'
+
 
 writeLines(model_string , con="model.txt" )
 
@@ -47,12 +51,18 @@ jags_model <- jags.model(file = "model.txt",
 samples <- coda.samples(jags_model,
                         variable.names = c("beta"),
                         n.iter = 10000,
-                        thin = 10
+                        thin = 5
                         )
 
+# model didnt converge, rerunning the models.
+#continued_samples <- coda.samples(jags_model,
+#                                  variable.names = c("beta"),
+#                                  n.iter = 20000, 
+#                                  thin = 2)
+
 save(samples, file = "posterior_samples.RData")
-posterior_df <- as.data.frame(as.mcmc.list(samples))
-write.csv(posterior_df, "posterior_samples.csv", row.names = FALSE)
+#posterior_df <- as.data.frame(as.mcmc.list(samples))
+#write.csv(posterior_df, "posterior_samples.csv", row.names = FALSE)
 
 gelman_result <- gelman.diag(samples)
 print(gelman_result)
@@ -61,44 +71,24 @@ print(gelman_result)
 summary(samples)
 plot(samples)
 
-
 ###### TEST SET PREDICTIONS
 #load("posterior_samples.RData")
 
-predict_proba <- function(beta_samples, X_row) {
-  if (is.vector(X_row)) {
-    X_row <- matrix(X_row, nrow = 1)
-  }
-  eta_samples <- as.matrix(beta_samples) %*% t(X_row) # calc linear predictors.
-  predicted_probabilities <- 1 / (1 + exp(-eta_samples))
-  return(predicted_probabilities)
-}
+posteria_means <- colMeans(as.matrix(samples))
 
-probabilities_row1 <- predict_proba(beta_samples, X_test[1, ])
-probabilities_row2 <- predict_proba(beta_samples, X_test[2, ])
+eta <- as.matrix(X_test) %*% posteria_means
+p_pred <- plogis(eta) # same as 1 / (1 + exp(-eta)) or exp(eta) / (1 + exp(eta))
+predictions <- ifelse(p_pred > 0.5, 1, 0)
 
-
-
-y_test <- filtered_test_data$target
-TP_encoded_test <- filtered_test_data$TP_encoded
-OP_encoded_test <- filtered_test_data$OP_encoded
-is_home_test <- filtered_test_data$is_home
-FT_TP_G_rolling_mean_test <- filtered_test_data$FT_TP_G_rolling_mean
-FT_OP_G_rolling_mean_test <- filtered_test_data$FT_OP_G_rolling_mean
-target_rolling_mean_test <- filtered_test_data$target_rolling_mean
-
-
-logit_p_1 <- samples[[1]][,1] + samples[[1]][,2]*TP_encoded_test[2]+samples[[1]][,3]*OP_encoded_test[2]+samples[[1]][,4]*is_home_test[2]+samples[[1]][,5]*FT_OP_G_rolling_mean_test[2]+samples[[1]][,6]*target_rolling_mean_test[2]
-logit_p_2 <- samples[[1]][,1] + samples[[1]][,2]*TP_encoded_test[3]+samples[[1]][,3]*OP_encoded_test[3]+samples[[1]][,4]*is_home_test[3]+samples[[1]][,5]*FT_OP_G_rolling_mean_test[3]+samples[[1]][,6]*target_rolling_mean_test[3]
-
-pred_1 = exp(logit_p_1)/(1+exp(logit_p_1))
-pred_2 = exp(logit_p_2)/(1+exp(logit_p_2))
+accuracy <- sum(predictions == y_test) / length(y_test)
+precision <- sum(predictions & y_test) / sum(predictions)
+print(paste("Accuracy:", accuracy))
+print(paste("Precision:", precision))
 
 library(ggplot2)
 
 pred_df = data.frame(pred_1, pred_2)
 names(pred_df) = c('home', 'away')
-
 
 ggplot(pred_df) +
   geom_histogram(aes(x = pred_1, y = ..density.., color = 'home_team', fill='home_team'),
@@ -108,3 +98,6 @@ ggplot(pred_df) +
 
 ##### FUTURE MATCHDAY 30 PREDICTIONS
 
+eta <- as.matrix(X_future) %*% posteria_means
+p_pred <- plogis(eta) # same as 1 / (1 + exp(-eta)) or exp(eta) / (1 + exp(eta))
+predictions <- ifelse(p_pred > 0.5, 1, 0)
